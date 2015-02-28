bd941fa6ec82dff253ed9e54620b9900b2cd85fe
cping.ju <zkp1985@gmail.com>
2015-02-28 22:15:59 +0800

    refactry compute the delta of data

--- analys/delta_collect.py
+++ analys/delta_collect.py
@@ -10,8 +10,9 @@ __author__ = 'keping.chu'
 from public.utils import tools
 
 from pyspark import SparkContext
-from pyspark.sql import HiveContext, Row, SQLContext
+from pyspark.sql import HiveContext
 from pyspark.mllib.regression import LabeledPoint, LinearRegressionWithSGD
+from operator import attrgetter
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -21,13 +22,14 @@ select_stock = "select split(rowkey, '_')[0] as date, code, price from hbase_sto
 
 def convert_to_point(data):
     #rows = [row for row in data[1]]
-    sorted_list = sorted(data[1], cmp=lambda frist, second: cmp(frist.date, second.date))
+    # sorted_list = sorted(data[1], cmp=lambda first, second: cmp(first.date, second.date))
+    sorted_list = sorted(data[1],  key=attrgetter('date'))
 
     point_list = []
     for row in sorted_list:
-        point_list.append(LabeledPoint(sorted_list.index(row) + 1, [row.price]))
+        point_list.append(LabeledPoint(sorted_list.index(row) + 1, [float(row.price)]))
 
-    return (data[0], point_list)
+    return (data[0], sorted_list, point_list)
 
 if __name__ == "__main__":
     sc = SparkContext(appName='delta collect')
@@ -37,11 +39,11 @@ if __name__ == "__main__":
         stocks = sqlContext.sql(select_stock.format(tools.day_after_now(day))).map(lambda row: (row.code, row)).groupByKey().map(convert_to_point)
         deltas = []
         for each_stock in stocks.collect():
-            data_set = sc.parallelize(each_stock[1])
+            data_set = sc.parallelize(each_stock[2])
             model = LinearRegressionWithSGD.train(data_set)
 
-            deltas.append((each_stock[0], model))
+            deltas.append((each_stock, model))
 
         for delta in deltas:
 
-            print delta[0], delta[1].weights
+            print delta[0][0],delta[0][1], delta[0][2], delta[1].weights
