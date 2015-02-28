be8343186fa8adbfb4bb190a3fcd31f3cba7f291
cping.ju <zkp1985@gmail.com>
2015-02-28 21:19:39 +0800

    refactry compute the delta of data

--- analys/delta_collect.py
+++ analys/delta_collect.py
@@ -18,9 +18,6 @@ sys.setdefaultencoding('utf8')
 
 select_stock = "select split(rowkey, '_')[0] as date, code, price from hbase_stock where split(rowkey, '_')[0] > '{0}'"
 
-sc = SparkContext(appName='delta collect')
-sqlContext = HiveContext(sc)
-
 
 def convert_to_point(data):
     #rows = [row for row in data[1]]
@@ -30,15 +27,21 @@ def convert_to_point(data):
     for row in sorted_list:
         point_list.append(LabeledPoint(sorted_list.index(row) + 1, [row.price]))
 
-    model = LinearRegressionWithSGD.train(sc.parallelize(each_stock[1]))
-
-    return (data[0], model)
-
+    return (data[0], point_list)
 
 if __name__ == "__main__":
-
+    sc = SparkContext(appName='delta collect')
+    sqlContext = HiveContext(sc)
     # data since month ago
     for day in [-30]:
         stocks = sqlContext.sql(select_stock.format(tools.day_after_now(day))).map(lambda row: (row.code, row)).groupByKey().map(convert_to_point)
+        data_sets = []
         for each_stock in stocks.collect():
-            print each_stock[0], each_stock[1].weights
+            data_set = sc.parallelize(each_stock[1])
+            data_sets.append((each_stock[0], data_set))
+
+        deltas = sc.parallelize(data_sets).map(lambda row: (row[0], LinearRegressionWithSGD.train(row[1])))
+
+        for delta in deltas.collect():
+
+            print delta[1].weights
