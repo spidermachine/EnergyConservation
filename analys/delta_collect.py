9f9fee37151b59285d7147d4ce7328a4863341dd
cping.chu <zkp1985@gmail.com>
2015-03-01 21:12:16 +0800

    compute delta

--- analys/delta_collect.py
+++ analys/delta_collect.py
@@ -18,7 +18,8 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-select_stock = "select split(rowkey, '_')[1] as date, code, price from hbase_stock where split(rowkey, '_')[1] > '{0}'"
+select_stock = "select split(rowkey, '_')[1] as date, code, name, delta_ratio from hbase_stock where split(rowkey, '_')[1] > '{0}'"
+select_stock = "select split(rowkey, '_')[1] as date, code, name, delta_ratio from hbase_stock where split(rowkey, '_')[1] > '{0}' and code = '600048'"
 
 
 def convert_to_point(data):
@@ -28,21 +29,25 @@ def convert_to_point(data):
 
     features = []
     labels = []
+    sum_amplitude = 0.0
+    name = ''
     for row in sorted_list:
-        labels.append(float(row.price))
+        name = row.name
+        sum_amplitude += float(row.delta_ratio.strip('%'))
+        labels.append(sum_amplitude)
         features.append([sorted_list.index(row) + 1])
 
     clf = linear_model.LinearRegression()
     clf.fit(features, labels)
-
-    return (data[0], sorted_list, clf.coef_, clf.intercept_)
+    return (data[0], name,  clf.coef_)
+    #return (data[0], sorted_list, clf.coef_, clf.intercept_)
 
 if __name__ == "__main__":
     sc = SparkContext(appName='delta collect')
     sqlContext = HiveContext(sc)
     # data since month ago
     for day in [-30]:
-        stocks = sqlContext.sql(select_stock.format(tools.day_after_now(day))).map(lambda row: (row.code, row)).groupByKey().map(convert_to_point)
+        stocks = sqlContext.sql(select_stock.format(tools.day_after_now(day))).map(lambda row: (row.code, row)).groupByKey().map(convert_to_point).sortBy(lambda row: row[2])
 
         for each_stock in stocks.collect():
-            print each_stock[0], each_stock[1], each_stock[2], each_stock[3]
+            print each_stock[0], each_stock[1], each_stock[2]
